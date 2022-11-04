ea25ee4d9159c44738e0b991f13ba50a37bf0598
Tapendra Sharma <ext.tapendra.sharma@openlm.com>
2022-11-04 14:38:40 +0530

    change in otp controller

--- src/controllers/otp.controller.js
+++ src/controllers/otp.controller.js
@@ -6,24 +6,30 @@ const otpGenerator = require('otp-generator')
 
 
 exports.create = (req,res) =>{
-  const otp =   '12345'//otpGenerator.generate(4, { upperCase: false,alphabets: false, specialChars: false ,digits:true});
-
-client.messages
-  .create({
-     body: 'Your OTP is '+otp,
-     from: '+17604725588',
-     to: req.body.MobileNumber
-   })
-  .then(message => 
+  //const otp =   '12345'//otpGenerator.generate(4, { upperCase: false,alphabets: false, specialChars: false ,digits:true});
     res.status(200).send({
-        status:true,
-        message:"otp send",
-        data:{
-            MessageSID : message.sid,
-            OTP : otp
+        status: true,
+        message: "otp send",
+        data: {
+            OTP: 1234
         }
-      })
-  )
+    })
+//client.messages
+//  .create({
+//     body: 'Your OTP is '+otp,
+//     from: '+17604725588',
+//     to: req.body.MobileNumber
+//   })
+//  .then(message => 
+//    res.status(200).send({
+//        status:true,
+//        message:"otp send",
+//        data:{
+//            MessageSID : message.sid,
+//            OTP : otp
+//        }
+//      })
+//  )
   .catch(err => {
         res.status(200).send({
           status:false,
